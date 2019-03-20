83a413757e61f1c6a5aa9391c9acfebc5f0d6154
kikocorreoso <kikocorreoso@gmail.com>
2019-03-20 19:38:16 +0100

    create package an __init__.py file

---
+++ spyder_black_formatter/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) kiko correoso
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Spyder black formatter Plugin."""
+
+
+from ._version import __version__
+
+# The following statements are required to register this 3rd party plugin:
+
+from .blackformatterplugin import BlackFormatterPlugin
+
+PLUGIN_CLASS = BlackFormatterPlugin
